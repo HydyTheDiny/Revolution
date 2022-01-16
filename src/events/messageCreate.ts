ffc379e1b8977c305d30a2b8b37a99046c8cc812
Jpuf0 <jpuf@jpuf.xyz>
2022-01-16 00:17:49 +0000

    changed some things

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,17 @@
+import ClientEvent from "@utils/ClientEvent";
+import mongoose, { Schema, model} from 'mongoose';
+
+export default new ClientEvent('messageCreate', async function(message) {
+    await mongoose.connect(process.env.DATABASE!);
+    const XPModel = model('XP', new Schema({
+      user: { type: String, default: message.author.id },
+      level: { type: Number, min: 1 },
+      xp: { type: Number, min: 0 }
+    }));
+
+    const userXP = new XPModel();
+    const a = userXP.save();
+
+    let exp = Math.floor()
+        
+});
