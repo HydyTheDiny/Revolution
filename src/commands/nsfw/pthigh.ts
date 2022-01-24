05b3f2145f2bdf94e2c2cb8e46ee0caa54422db5
Hydra 6 <Hydythediny@gmail.com>
2022-01-24 14:16:32 -0700

    fixed api error

--- src/commands/nsfw/pthigh.ts
+++ src/commands/nsfw/pthigh.ts
@@ -33,7 +33,7 @@ export default new Command("pthigh")
   .setExecutor(async function(msg) {
     if(msg.channel.nsfw === true){
         let data = await req("https://nekobot.xyz/api/image")
-        .query("type", "pthigh" ).json<{success: boolean, message: string, color: number, version: string}>()
+        .query("type", "thigh" ).json<{success: boolean, message: string, color: number, version: string}>()
       if (!data) return msg.reply('The API returned an error.')
       if (!data.success) return msg.reply('The API returned an error.')
       return msg.reply({
