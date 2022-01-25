0397833febb7ce6fb846455dfc1c470c897a5da0
Hydra 6 <Hydythediny@gmail.com>
2022-01-24 17:52:03 -0700

    removed mongoose

--- src/util/economy.ts
+++ src/util/economy.ts
@@ -1,17 +1,7 @@
 import ExtendedMessage from "@util/ExtendedMessage";
 import ClientEvent from "@util/ClientEvent";
-import mongoose from 'mongoose';
+
 
 export default new ClientEvent('messageCreate', async function (message) {
-    if(message){
-        await mongoose.connect("mongodb+srv://Revolution:Revolution.Database.Admin.Access.Allowed@cluster0.am9ut.mongodb.net/DiscordDatabase?retryWrites=true&w=majority",{
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          userFindAndModify: false
-        }).then(() => {
-          console.log('Connected to the Database');
-        }).catch((err) => {
-          console.log(err)
-        })
-  } else return;
+
 });
