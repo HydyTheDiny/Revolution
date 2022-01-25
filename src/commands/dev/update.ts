62a448ae41167f5d3455a10c7b55952eec657002
Jpuf <38541170+Jpuf0@users.noreply.github.com>
2022-01-25 10:45:24 +0000

    actually assign variables

--- src/commands/dev/update.ts
+++ src/commands/dev/update.ts
@@ -9,12 +9,13 @@ export default new Command("update")
 		try {
 			const out = execSync("git pull").toString();
 			let file;
+			if (out.length >= 950) file = out;
 			const latest = execSync("git log -1").toString().split("\n").map(line => `> ${line}`);
 			latest[3] = "> Commit Message:";
 			if (latest.length === 6) latest.splice(5);
 			const noChanges = out.includes("Already up to date.");
 			const exit = msg.dashedArgs.value.includes("exit");
-			await msg.reply(`Success. ${noChanges ? `No changes were made. ${exit ? " Not exiting." : ""}` : `${exit ? " Exiting in 2 seconds." : " Not exiting."}\n\nCommit Info:\n${latest.join("\n")}`}${(file === undefined) ? '\n\`\`\`sh\nOutput:\n${out}\`\`\`' : undefined}`, (file === undefined) ? undefined : { file: out, name: 'output.txt'})
+			await msg.reply(`Success. ${noChanges ? `No changes were made. ${exit ? " Not exiting." : ""}` : `${exit ? " Exiting in 2 seconds." : " Not exiting."}\n\nCommit Info:\n${latest.join("\n")}`}${(file === undefined) ? '\n\`\`\`sh\nOutput:\n${out}\`\`\`' : undefined}`, (file === undefined) ? undefined : { file: file, name: 'output.txt'})
 			await msg.reply();
 			await msg.channel.sendTyping();
 			if (exit && !noChanges) setTimeout(() => process.exit(0), 2e3);
