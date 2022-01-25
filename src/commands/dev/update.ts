80e482ea5b33b63ff4090a4048a7a88b95a304f0
Jpuf <38541170+Jpuf0@users.noreply.github.com>
2022-01-25 11:00:09 +0000

    template literals

--- src/commands/dev/update.ts
+++ src/commands/dev/update.ts
@@ -15,7 +15,7 @@ export default new Command("update")
 			if (latest.length === 6) latest.splice(5);
 			const noChanges = out.includes("Already up to date.");
 			const exit = msg.dashedArgs.value.includes("exit");
-			await msg.reply(`Success. ${noChanges ? `No changes were made. ${exit ? " Not exiting." : ""}` : `${exit ? " Exiting in 2 seconds." : " Not exiting."}\n\nCommit Info:\n${latest.join("\n")}`}${(file === undefined) ? '\n\`\`\`sh\nOutput:\n${out}\`\`\`' : undefined}`, (file === undefined) ? undefined : { file: file, name: 'output.txt'})
+			await msg.reply(`Success. ${noChanges ? `No changes were made. ${exit ? " Not exiting." : ""}` : `${exit ? " Exiting in 2 seconds." : " Not exiting."}\n\nCommit Info:\n${latest.join("\n")}`}${(file === undefined) ? `\n\`\`\`sh\nOutput:\n${out}\`\`\`` : undefined}`, (file === undefined) ? undefined : { file: file, name: 'output.txt'})
 			await msg.channel.sendTyping();
 			if (exit && !noChanges) setTimeout(() => process.exit(0), 2e3);
 		} catch (e) {
